d05b703c77ca7d2f76cc9f992b8d07fe85a66f10
Joshua Burger <joshberger6969@outlook.com>
2022-10-18 21:00:36 -0400

    Method Overloading Java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("New score is " + newScore);
         calculateScore(75);
         calculateScore();
+        calcFeetAndInchesToCentimeters(6, 0);
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -19,5 +20,17 @@ public class Main {
     public static int calculateScore() {
         System.out.println("No player name, no player score");
         return 0;
+    }
+
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if((feet < 0) || (inches < 0) && (inches > 12)) {
+            return -1;
+        }
+
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
+        return centimeters;
     }
 }
